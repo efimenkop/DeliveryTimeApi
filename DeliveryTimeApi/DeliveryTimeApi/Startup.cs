b852501901fa354e66c9d4ad9e2ef96080eea66d
Pavel Efimenko <efimenkopavel1985@gmail.com>
2020-04-26 14:54:44 +0300

    add kestrel compatibility

--- DeliveryTimeApi/DeliveryTimeApi/Startup.cs
+++ DeliveryTimeApi/DeliveryTimeApi/Startup.cs
@@ -1,8 +1,10 @@
+using System.Net;
 using System.Text.Json.Serialization;
 using DeliveryTimeApi.Repositories;
 using DeliveryTimeApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +23,11 @@ namespace DeliveryTimeApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
+            });
+
             services.AddSingleton<IDeliveryTimeRepository, DeliveryTimeInMemoryRepository>();
             services.AddSingleton<IDeliveryTimeService, DeliveryTimeService>();
             services.AddControllers()
@@ -35,6 +42,11 @@ namespace DeliveryTimeApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
